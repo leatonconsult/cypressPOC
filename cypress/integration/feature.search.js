f61cd9b4781b2b2bc95a9deed4d70979702c4365
Josh Taylor <josh@joshs-mbp.home>
2021-11-18 16:45:40 +0000

    added date -1 day +3 years

--- cypress/integration/feature.search.js
+++ cypress/integration/feature.search.js
@@ -7,12 +7,12 @@
 
 import '../support/search.js'
 import '../support/utils.js'
-import { todaysDate2Years } from "../support/utils";
+import { todaysDate3Years } from "../support/utils";
 
 describe('Search', () => {
   it('Expect no results', () => {
 	  //var searchStr = 'Mens Shirt'
-	  const searchStr = todaysDate2Years();
+	  const searchStr = todaysDate3Years();
 	  cy.search(searchStr)
 	  cy.get('[class="alert alert-warning"]').should('include.text', searchStr)
 	  cy.contains('0 results have been found')
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -18,9 +18,18 @@ function todaysDate() {
 
 
 /**
- * get date using datejs as dd-MMM-yyyy (18-Nov-2021)
+ * get date using datejs +2 years as dd-MMM-yyyy (18-Nov-2021)
  */
 function todaysDate2Years() { 
 	const today = Date.today().add(2).years().toString("dd-MMM-yyyy")
 	return today;
 } export { todaysDate2Years };
+
+
+/**
+ * get date using datejs -1 day +3 yearsas dd-MMM-yyyy (18-Nov-2021)
+ */
+function todaysDate3Years() { 
+	const today = Date.today().add(-1).days().add(3).years().toString("dd-MMM-yyyy")
+	return today;
+} export { todaysDate3Years };
