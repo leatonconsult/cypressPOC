737a87a8c6b7a0ad3c638e771ae3b434bca506a7
Josh Taylor <josh.taylor@leatonconsult.co.uk>
2022-02-01 14:16:49 +0000

    First API example test

--- cypress/integration/api/feature.firstTest.js
+++ cypress/integration/api/feature.firstTest.js
@@ -1,11 +1,20 @@
 // import { getDate } from "../support/date";
 
-describe("todos", () => {
-  it("GET - to do items", () => {
-	  cy.request('/todos/').as('todoRequest');
-	  cy.get('@todoRequest').then(todos => {
-		  expect(todos.status).to.eq(200);
-		  assert.isArray(todos.body, 'Todos Response is an array')
-	  });
-  });
-});
+let ticket
+describe("auth", () => {
+  it("get auth ticket", () => {
+	  cy.request({
+	        method: 'POST',
+	        url: Cypress.env('api_identity_url'), //get from cypress.env.json
+	        form: true, //sets to application/x-www-form-urlencoded
+	        username: Cypress.env('api_identity_username'),
+	        password: Cypress.env('api_identity_password')
+	  	})
+	        .its('body')
+	        .then((response) => {
+	            ticket = response
+	            window.localStorage.setItem('ticket', JSON.stringify(ticket))
+	            cy.log(identity)
+	    })
+  	})
+})
